73afaf22727c45f68eb6a66ea71c533cf058b0e0
jamesdoom <brianpalmer39@gmail.com>
2022-11-04 07:54:51 -0400

    Update calculator functions to include: add, subtract, sum, multiply, power, factorial.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+function add(num1, num2) {
+	addTotal=num1+num2;
+  return addTotal;
 };
 
-const subtract = function() {
-	
+function subtract(num1, num2) {
+	subTotal=num1-num2;
+  return subTotal;
 };
 
-const sum = function() {
-	
+function sum(nums) {
+  const initial = 0;
+  const sumTotal = initial + nums.reduce(
+    (previous, current) => previous + current,
+    initial)
+  return sumTotal;
 };
 
-const multiply = function() {
-
+function multiply(nums) {
+  const mulTotal = nums.reduce(
+    (previous, current) => previous * current)
+    return mulTotal;
 };
 
-const power = function() {
-	
+function power(num1, num2){
+  const powTotal = num1 ** num2;
+    return powTotal;
 };
 
-const factorial = function() {
-	
+function factorial(num) {
+  if (num === 0 || num === 1)
+  return 1;
+  for (i = num - 1; i >= 1; i--){
+    num *= i;
+  }
+return num;
 };
 
 // Do not edit below this line
